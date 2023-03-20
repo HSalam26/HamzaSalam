3b16a3d594a4f9809f3cdd5a235983cdf56488ad
HSalam26 <128044690+HSalam26@users.noreply.github.com>
2023-03-20 11:00:36 +0000

    Add files via upload

---
+++ maintesting.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+module "web_app" {
+  source = "../../Terraform/Testing"
+}
+
+output "instance_ip_addr" {
+  value = module.web_app.instance_ip_addr
+}
+
+output "url" {
+  value = "http://${module.web_app.instance_ip_addr}:8080"
+}
